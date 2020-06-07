acf52ed0bd61bb38c836a3cafda80a22182a4949
Nicolas Pauss <nicolas.pauss@gmail.com>
2020-06-07 10:52:09 +0200

    Move .env to new env 3 to avoid loading variables messages in other tests.

--- tests/dotenv_load_test.sh
+++ tests/dotenv_load_test.sh
@@ -10,11 +10,11 @@ TEST_DIR="$(dirname -- "$TEST_SCRIPT")"
 . "$TEST_DIR/test_helpers"
 
 
-ENV_1_VAR_A="foo"
-ENV_1_VAR_B="bar"
-ENV_1_VAR_C=""
-ENV_1_VAR_D=""
-ENV_1_VAR_E="toto
+ENV_3_VAR_A="foo"
+ENV_3_VAR_B="bar"
+ENV_3_VAR_C=""
+ENV_3_VAR_D=""
+ENV_3_VAR_E="toto
 titi"
 
 
@@ -27,13 +27,13 @@ get_env_var() {
 
 
 test_pipenv_run() {
-    # Change directory to env 1 and check dotenv variables.
-    cd -- "$TEST_ENVS_TMPDIR/1" || fail "cd to env 1"
-    assertEquals "VAR A in env 1" "$ENV_1_VAR_A" "$(get_env_var "VAR_A" 'pipenv run')"
-    assertEquals "VAR B in env 1" "$ENV_1_VAR_B" "$(get_env_var "VAR_B" 'pipenv run')"
-    assertEquals "VAR C in env 1" "$ENV_1_VAR_C" "$(get_env_var "VAR_C" 'pipenv run')"
-    assertEquals "VAR D in env 1" "$ENV_1_VAR_D" "$(get_env_var "VAR_D" 'pipenv run')"
-    assertEquals "VAR E in env 1" "$ENV_1_VAR_E" "$(get_env_var "VAR_E" 'pipenv run')"
+    # Change directory to env 3 and check dotenv variables.
+    cd -- "$TEST_ENVS_TMPDIR/3" || fail "cd to env 3"
+    assertEquals "VAR A in env 3" "$ENV_3_VAR_A" "$(get_env_var "VAR_A" 'pipenv run')"
+    assertEquals "VAR B in env 3" "$ENV_3_VAR_B" "$(get_env_var "VAR_B" 'pipenv run')"
+    assertEquals "VAR C in env 3" "$ENV_3_VAR_C" "$(get_env_var "VAR_C" 'pipenv run')"
+    assertEquals "VAR D in env 3" "$ENV_3_VAR_D" "$(get_env_var "VAR_D" 'pipenv run')"
+    assertEquals "VAR E in env 3" "$ENV_3_VAR_E" "$(get_env_var "VAR_E" 'pipenv run')"
 
     # Change directory to env 2 and check dotenv variables.
     cd -- "$TEST_ENVS_TMPDIR/2" || fail "cd to env 2"
